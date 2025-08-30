38c8e804b60c81ee3a2a23eba03bb1b4c560728f
yejot1514 <banchihai@gmail.com>
2025-08-29 21:49:42 -0400

    first commit of cartItem

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,33 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
+    return cart.reduce((total, item) => total + parseFloat(item.cost.substring(1)) * item.quantity, 0);
  
   };
 
   const handleContinueShopping = (e) => {
+    onContinueShopping(e);
    
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return parseFloat(item.cost.substring(1)) * item.quantity;
+
   };
 
   return (
